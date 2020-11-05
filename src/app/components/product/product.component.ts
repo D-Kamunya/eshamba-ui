11a456de98df255ff2b8262263a4eb18318ec53f
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-05 23:12:12 +0300

    Create product service get products page

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product-service/product.service'
+
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _productService: ProductService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  	this._productService.getProducts()
   }
 
 }
---
+++ src/app/services/product-service/product.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../../environments/environment'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+
+
+  private httpOptions: any;
+  products:any
+  errors:boolean=false
+  
+
+
+  constructor(private http:HttpClient) {
+	  this.httpOptions = {
+	      headers: new HttpHeaders({'Content-Type': 'application/json'})
+	    }
+  }
+
+
+
+    public getProducts() {
+    this.http.get(`${environment.api_url}/shamba/api/product`, this.httpOptions).subscribe(
+      data => {
+        this.products=data
+        console.log(data)       
+      },
+      err => {
+        console.log(err)
+        this.errors=true
+          }
+    );
+  }
+}
