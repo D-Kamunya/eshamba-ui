24de7365aeeb6d5539fca422e58431b4c6f668f2
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-05 08:54:55 +0300

    Create register component with register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import {ForumComponent} from './components/forum/forum.component'
 import {ManageCropsComponent} from './components/manage-crops/manage-crops.component'
 import {ScheduleComponent} from './components/schedule/schedule.component'
 import {LoginComponent} from './components/login/login.component'
-
+import {RegisterComponent} from './components/register/register.component'
 const routes: Routes = [
 	{path:'',component:HomeComponent},
 	{path:'login',component:LoginComponent},
+	{path:'register',component:RegisterComponent},
   	{path:'products',component:ProductComponent},
   	{path:'schedules', component: ScheduleComponent },
   	{path:'forum',component:ForumComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ScheduleComponent } from './components/schedule/schedule.component';
 import { ForumComponent } from './components/forum/forum.component';
 import { ManageCropsComponent } from './components/manage-crops/manage-crops.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { LoginComponent } from './components/login/login.component';
     ScheduleComponent,
     ForumComponent,
     ManageCropsComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
