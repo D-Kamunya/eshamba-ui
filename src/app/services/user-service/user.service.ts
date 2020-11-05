bdf7cb98b0715a0ccd631873481ab16fa6347eff
Dennis Kamunya <dennismuriithik@gmail.com>
2020-11-05 06:29:52 +0300

    Create userservice

---
+++ src/app/services/user-service/user.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+	// http options used for making API calls
+  private httpOptions: any;
+ 
+  // the actual JWT token
+  public token: string;
+
+	// the  JWT refresh token
+  public refresh_token: string;
+ 
+  // the token expiration date
+  public token_expires: Date;
+ 
+  // the username of the logged in user
+  public user_id: int;
+ 
+  // error messages received from the login attempt
+  public errors: any = [];
+
+  constructor() {
+  	this.httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    }
+   }
+
+  // Uses http.post() to get an auth token from drf-jwt endpoint
+  public login(user) {
+    this.http.post('/api/token/', JSON.stringify(user), this.httpOptions).subscribe(
+      data => {
+        this.updateData(data['access']);
+        this.refresh_token=data['refresh']
+      },
+      err => {
+        this.errors = err['error'];
+      }
+    );
+  }
+ 
+  // Refreshes the JWT token, to extend the time the user is logged in
+  public refreshToken() {
+    this.http.post('/api/token/refresh/', JSON.stringify({token: this.refresh_token}), this.httpOptions).subscribe(
+      data => {
+        this.updateData(data['token']);
+      },
+      err => {
+        this.errors = err['error'];
+      }
+    );
+  }
+ 
+  public logout() {
+    this.token = null;
+    this.token_expires = null;
+    this.username = null;
+  }
+ 
+  private updateData(token) {
+    this.token = token;
+    this.errors = [];
+ 
+    // decode the token to read the username and expiration timestamp
+    const token_parts = this.token.split(/\./);
+    const token_decoded = JSON.parse(window.atob(token_parts[1]));
+    this.token_expires = new Date(token_decoded.exp * 1000);
+    this.username = token_decoded.username;
+  }
+ 
+}
